401de8034d591fd430ec0dbbda9be41055153439
bloldf <48572660+bloldf@users.noreply.github.com>
2019-11-13 18:55:53 +0100

    Add files via upload

---
+++ .bash_history
@@ -0,0 +1,2 @@
+wget https://cdn.glitch.com/86418dd3-b67c-46bb-9666-d8b33da0d226%2Fimage0-2.png
+refresh
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NjIyNzc3MjkwMjExNzIxMjM2.XcLiIA.jeQY4ADv_Cury_AYvI2V-pBVca4",
+  YT_API_KEY: "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4",
+  prefix: "#",
+  devs: ["488284776713945088"]
+};
